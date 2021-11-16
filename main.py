5e6e86d3f13df4bf6d1bfd02518ff01ac84eb9d7
Mohammad Mohsen <kuro.ece@gmail.com>
2021-11-16 17:40:03 +0200

    Changes: 1- Fixed `open file` and `close file` so that the same file won't be opened in multiple tabs 2- Added logging 3- Added `closeEvent`, so that the editor can be closed safely through the [X] button and the shortcut (CTRL+Q)

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 import os
 import pathlib
 import sys
-import typing
+import logging as log
+import inspect
 
 from PySide2.QtCore import *
 from PySide2.QtGui import *
@@ -22,14 +22,14 @@ class MainWindow(QMainWindow):
 
         super(MainWindow, self).__init__(*args, **kwargs)
 
+        self.logger: log.Logger = log.getLogger(self.__class__.__name__)
+
         # ----------------------------------------------------------------------
         # --------------------------- GUI components ---------------------------
         # ----------------------------------------------------------------------
 
         # text editor
         self.container: QTabWidget = QTabWidget()  # container (tab widget)
-
-        self.current_open_files: typing.Dict[str, int] = dict()
 
         self.status_bar: QStatusBar = QStatusBar()  # status bar
         self.file_menu: QMenu = QMenu("&File")  # file menu
@@ -41,7 +41,8 @@ class MainWindow(QMainWindow):
         self.container.setTabsClosable(True)  # set tabs closable
         self.container.setMovable(True)  # set tabs movable
         self.container.setDocumentMode(True)  # set document mode
-        self.container.tabCloseRequested.connect(self.close_current_tab)  # connect tab close request signal
+        self.container.tabCloseRequested.connect(self.close_tab)  # connect tab close request signal
+        self.container.currentChanged.connect(self.current_tab_changed)  # connect current tab changed
 
         # set container as central widget
         self.setCentralWidget(self.container)
@@ -128,7 +129,7 @@ class MainWindow(QMainWindow):
         # close tab action
         close_tab_action = QAction("Close current tab", self)
         close_tab_action.setStatusTip("Close current tab")
-        close_tab_action.triggered.connect(self.close_tab)
+        close_tab_action.triggered.connect(self.close_current_tab)
         close_tab_shortcut = QKeySequence(Qt.CTRL + Qt.Key_W)
         close_tab_action.setShortcut(close_tab_shortcut)
         self.file_menu.addAction(close_tab_action)
@@ -215,12 +216,6 @@ class MainWindow(QMainWindow):
         # add status bar
         self.setStatusBar(self.status_bar)
 
-        # ----------------------------------------------------------------------
-        # -------------------------- show main window --------------------------
-        # ----------------------------------------------------------------------
-        # show main window
-        self.update_title()
-
         # create new file
         self.create_new_file()
 
@@ -280,6 +275,7 @@ class MainWindow(QMainWindow):
 
         if editor_path is None:
             editor_title = f"new file * | {editor_title}"
+            open_file_name = "new file *"
             self.container.tabBar().setTabTextColor(self.container.currentIndex(), Qt.red)
         elif current_editor.is_modified():
             editor_title = f"{str(editor_path)} * | {editor_title}"
@@ -291,51 +287,82 @@ class MainWindow(QMainWindow):
         self.setWindowTitle(editor_title)
         self.container.setTabText(self.container.currentIndex(), open_file_name)
 
+        caller = inspect.currentframe().f_back.f_code.co_name
+        self.logger.debug(f"{caller}: Updating title for {open_file_name=}")
+
     def create_new_file(self) -> None:
         """new editor file"""
-        self.add_editor()
+        self.add_editor_tab()
         self.get_current_editor().create_new_file()
         self.get_current_editor().setFocus()
-        file_name: str = self.get_current_editor().get_open_file_name()
-        self.current_open_files[file_name] = self.container.currentIndex()
         self.update_title()
+        caller = inspect.currentframe().f_back.f_code.co_name
+        self.logger.debug(f"{caller}: New file created in tab: {self.container.currentIndex()}")
 
     def open_file(self) -> None:
         """Open an existing file"""
+        caller = inspect.currentframe().f_back.f_code.co_name
+        self.logger.debug(f"{caller}: Opening a file")
+
         current_editor: MindustryLogicEditor = self.get_current_editor()
 
+        # check if there is an open file (not a new file)
         if current_editor.path is not None:
-            self.add_editor()
+            self.add_editor_tab()
+            current_editor: MindustryLogicEditor = self.get_current_editor()
 
-        self.get_current_editor().open_file()
-        file_name: str = self.get_current_editor().get_open_file_name()
-        file_tab: int = self.current_open_files.get(file_name, None)
+        # open file
+        current_editor.open_file()
 
-        if file_tab is not None:
-            self.container.removeTab(file_tab)
+        # open was canceled
+        if current_editor.path is None:
+            self.logger.debug(f"Open file as cancelled")
+            return
 
-        self.current_open_files[file_name] = self.container.currentIndex()
-        self.get_current_editor().setFocus()
-        self.update_title()
+        # get current file name & tab index
+        file_name: str = current_editor.get_open_file_name()
+        current_tab: int = self.container.currentIndex()
+
+        # check if there is a file already opened with the same name
+        open_tabs_count: int = self.container.count()
+        for tab_index in range(open_tabs_count):
+
+            # skip current tab
+            if tab_index == current_tab:
+                continue
+
+            tab_editor: MindustryLogicEditor = self.container.widget(tab_index)  # editor in the tab
+            tab_file: str = tab_editor.get_open_file_name()  # file open in the tab
+            if tab_file == file_name:
+                self.logger.debug(f"file {file_name} is already open in tab: {tab_index}")
+                self.container.removeTab(tab_index)  # close tab & its editor
+                break
+
+        current_editor.setFocus()  # set focus on current tab editor
+        self.update_title()  # update window title
+        self.logger.debug(f"{caller}:File opened: {file_name} in tab {current_tab}")
 
     def save_file(self) -> None:
         """Save current open file"""
         self.get_current_editor().save_file()
         self.update_title()
+        caller = inspect.currentframe().f_back.f_code.co_name
+        self.logger.debug(f"{caller}: File saved")
 
     def save_file_as(self) -> None:
         """Save current file as another file"""
         self.get_current_editor().save_file_as()
         self.update_title()
+        caller = inspect.currentframe().f_back.f_code.co_name
+        self.logger.debug(f"{caller}: File save as")
 
-    def add_editor(self) -> None:
+    def add_editor_tab(self) -> None:
         """
         Add an editor in a new tab
 
         :return: None
         :rtype: None
         """
-
         # create a new editor instance
         editor = MindustryLogicEditor()
 
@@ -350,6 +377,8 @@ class MainWindow(QMainWindow):
         self.bind_editor_shortcuts(editor)
         self.container.setCurrentIndex(tab_index)
         editor.setFocus()
+        caller = inspect.currentframe().f_back.f_code.co_name
+        self.logger.debug(f"{caller}: Added a new editor in tab: {tab_index}")
 
     def get_current_editor(self) -> MindustryLogicEditor:
         """
@@ -360,15 +389,20 @@ class MainWindow(QMainWindow):
         """
         return self.container.currentWidget()
 
-    def close_tab(self) -> None:
+    def close_current_tab(self) -> None:
         """
         Close current open tab
 
         :return: None
         :rtype: None
         """
+
         current_editor: MindustryLogicEditor = self.get_current_editor()
         current_file_name: str = current_editor.get_open_file_name()
+
+        caller = inspect.currentframe().f_back.f_code.co_name
+        self.logger.debug(f"{caller}: Closing current tab {self.container.currentIndex()}: {current_file_name}")
+
         if current_editor.is_modified():
             confirm_save: int = self.confirm_message(f"Save file {current_file_name} before closing?")
             if confirm_save == QMessageBox.Cancel:
@@ -378,8 +412,7 @@ class MainWindow(QMainWindow):
             else:  # QMessageBox.No
                 pass  # don't save file
 
-        del self.current_open_files[current_file_name]
-        self.close_current_tab(self.container.currentIndex())
+        self.close_tab(self.container.currentIndex())
 
     def close_editor(self) -> None:
         """
@@ -388,16 +421,6 @@ class MainWindow(QMainWindow):
         :return: None
         :rtype: None
         """
-
-        close_confirm: int = self.confirm_message(f"Any unsaved files will be discarded. Confirm closing the editor?")
-
-        if close_confirm in (QMessageBox.Cancel, QMessageBox.No):
-            return
-
-        for tab_index in range(self.container.count()):
-            tab_editor: MindustryLogicEditor = self.container.widget(tab_index)
-            tab_editor.close()
-
         self.close()
 
     def switch_tab(self) -> None:
@@ -410,9 +433,26 @@ class MainWindow(QMainWindow):
         current_tab_index: int = self.container.currentIndex()
         new_tab_index: int = (current_tab_index + 1) % self.container.count()
         self.container.setCurrentIndex(new_tab_index)
+        caller = inspect.currentframe().f_back.f_code.co_name
+        self.logger.debug(f"{caller}: User switching tabs from {current_tab_index} to {new_tab_index}")
+
+    def closeEvent(self, event: QCloseEvent) -> None:
+        close_confirm: int = self.confirm_message(f"Any unsaved files will be discarded. Confirm closing the editor?")
+
+        if close_confirm in (QMessageBox.Cancel, QMessageBox.No):
+            return
+
+        for tab_index in range(self.container.count()):
+            tab_editor: MindustryLogicEditor = self.container.widget(tab_index)
+            tab_editor.close()
+
+        caller = inspect.currentframe().f_back.f_code.co_name
+        self.logger.debug(f"{caller}: Closing the editor")
+
+        super(MainWindow, self).closeEvent(event)
 
     @Slot(int)
-    def close_current_tab(self, tab_index: int) -> None:
+    def close_tab(self, tab_index: int) -> None:
         """
         Close current open tab
 
@@ -421,9 +461,8 @@ class MainWindow(QMainWindow):
         :return: None
         :rtype: None
         """
-        self.get_current_editor().close()
+        self.container.widget(tab_index).close()
         self.container.removeTab(tab_index)
-
         if self.container.count() < 1:
             self.create_new_file()
 
@@ -432,8 +471,20 @@ class MainWindow(QMainWindow):
         if changed:
             self.update_title()
 
+    @Slot(int)
+    def current_tab_changed(self, new_tab_index: int) -> None:
+        """Current tab was changed"""
+        if new_tab_index > -1:
+            self.update_title()
+
 
 if __name__ == "__main__":
+    log.basicConfig(
+        stream=sys.stdout,
+        level=log.DEBUG,
+        format="[%(name)s] %(funcName)s:%(lineno)d %(message)s"
+    )
+
     app = QApplication(sys.argv)
     app.setWindowIcon(QIcon("images\\icon.ico"))
     win = MainWindow()
